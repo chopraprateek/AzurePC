ff0f3026f82ae08360677fca9c3dc7e6e34e5474
chopraprateek <chopra.prateek21@gmail.com>
2024-08-22 11:52:55 +0530

    first commit

---
+++ src/main/java/com/cloud/azure/rest/FunRestController.java
@@ -0,0 +1,18 @@
+package com.cloud.azure.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String Hello(){
+        return "Hello..How are you?";
+    }
+
+    @GetMapping("/startnow")
+    public String HelloWow(){
+        return "I am good?";
+    }
+}
